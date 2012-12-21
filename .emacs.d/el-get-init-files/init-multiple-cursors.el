cc27ad62bcdf0e5476b4c9b59ae484cbee8b00bc
Aphrodite Spartacus <Aphrodite.Spartacus@gmail.com>
2012-12-21 23:47:25 +0800

    fighting

---
+++ .emacs.d/el-get-init-files/init-multiple-cursors.el
@@ -0,0 +1,6 @@
+(require 'multiple-cursors)
+
+(global-set-key (kbd "C-c m l") 'mc/edit-lines)
+(global-set-key (kbd "C-c m a") 'mc/mark-all-like-this)
+(global-set-key (kbd "C-c m n") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-c m p") 'mc/mark-previous-like-this)
